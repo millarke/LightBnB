7ae1e1ce7fa8f155e159b17cbb9edfbee8ba4841
Keith Millar <keithmillar@hotmail.ca>
2020-07-04 17:06:51 +0000

    switched reservations to SQL query, cleaned up semicolons and spaces in apiRoutes

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -131,7 +131,8 @@ exports.addUser = addUser;
 // };
 
 const getAllReservations = function(guest_id, limit = 10) {
-  const query = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+  const query = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id 
@@ -139,12 +140,12 @@ WHERE reservations.guest_id = $1
 AND reservations.end_date < now()::date
 GROUP BY properties.id, reservations.id
 ORDER BY reservations.start_date
-LIMIT ${limit}`;
-  return db.query(query, [guest_id])
+LIMIT $2
+`;
+  return pool.query(query, [guest_id, limit])
     .then(res => res.rows)
     .catch(err => console.error(err));
 };
-
 
 exports.getAllReservations = getAllReservations;
 
